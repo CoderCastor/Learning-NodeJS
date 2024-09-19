88c2fb7c610976975161ba4b10e779bb6bd18fc2
codercastor <tejasshinde935@gmail.com>
2024-09-19 22:55:49 +0530

    Hello world node js

---
+++ L1/hello.js
@@ -0,0 +1,8 @@
+console.log("Hey there! I'm Castor");
+
+
+//Using npm init
+
+//edit scripts in package.json file
+//added ------->>>   start:"node hello.js"
+//run       npm start       for running file
