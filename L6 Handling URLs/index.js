674fbc83c649b341f1806164d4f8ec1affe463a6
codercastor <tejasshinde935@gmail.com>
2024-09-20 20:46:21 +0530

    Handling URLs learned

---
+++ L6 Handling URLs/index.js
@@ -0,0 +1,45 @@
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+
+const myServer = http.createServer((req,res)=>{
+
+    //for ignoring favicon entry
+    if(req.url === '/favicon.ico') return res.end();
+
+    const log = `${Date.now()}: ${req.url} New_Req_Received\n`;
+
+    //writing about url
+    const myUrl = url.parse(req.url, true)   //this true separates the parameters into objects
+    console.log(myUrl);
+    
+
+    fs.appendFile("output.txt",log, (err)=>{
+        
+       let msg; 
+        switch(myUrl.pathname){
+            case "/": 
+                msg = "Homepage"
+                break;
+            case "/about": 
+                const username = myUrl.query.username;
+                msg = `Hello! ${username}`;
+                break;
+            case "/search":
+                const search = myUrl.query.search_query;
+                res.end("Here are your results for " + search);
+                break;
+
+            default: msg = "404 Not Found"; 
+           
+        }
+        res.end(msg);
+
+    })
+
+})
+
+myServer.listen(8000, ()=>{
+    console.log("Server is started !");
+    
+})
