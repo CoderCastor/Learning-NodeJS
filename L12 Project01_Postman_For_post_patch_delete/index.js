1edcba770b12aac94d8ed5dded7aeb93a85e228e
codercastor <tejasshinde935@gmail.com>
2024-09-21 17:29:17 +0530

    Learned POST method with Postman

--- L12 Project01_Postman_For_post_patch_delete/index.js
+++ L12 Project01_Postman_For_post_patch_delete/index.js
@@ -1,9 +1,14 @@
 const express = require("express")
-const users = require('./MOCK_DATA.json')
+const fs = require("fs");
+const users = require('../L11 Project01/MOCK_DATA.json')
 
 const app = express();
 const port = 8000;
 
+//middleware - plugin (without this express not understand what type of data is getting from postman)
+app.use(express.urlencoded({extended: false}));
+
+//Routes
 app.get("/users",(req,res)=>{
     //throwing users
     const html = `
@@ -14,8 +19,6 @@ app.get("/users",(req,res)=>{
 
     res.send(html);
 })
-
-
 
 //REST API
 app.get("/api/users",(req,res)=>{
@@ -31,12 +34,21 @@ app.get("/api/users/:id",(req,res)=>{
 
 app.post("/api/users",(req,res)=>{
     //TODO: create new user
-    return res.json({ status: "pending" });
+    const body = req.body;
+    users.push({...body,id: users.length + 1})
+    fs.writeFile('./MOCK_DATA.json',JSON.stringify(users),(err,data)=>{
+
+        return res.json({ status: "success", id: users.length});
+    })
 });
 
 
 app.patch("/api/users/:id",(req,res)=>{
     //TODO: edit the user with id
+    
+
+
+
     return res.json({ status: "pending" });
 });
 
@@ -47,22 +59,22 @@ app.delete("/api/users/:id",(req,res)=>{
 
 
 // grouping of routes for same route /api/users/:id
-app
-    .route("/api/users/:id")
-    .get(
-        (req,res)=>{
-            const id = Number(req.params.id);
-            const user = users.find((user)=> user.id === id);
-            return res.json(user);
-        }
-    )
-   .patch((req,res)=>{
-        //TODO: edit the user with id
-        return res.json({ status: "pending" });
-    })
-    .delete((req,res)=>{
-        //TODO: Delete the user with id
-        return res.json({ status: "pending" });
-    })
+// app
+//     .route("/api/users/:id")
+//     .get(
+//         (req,res)=>{
+//             const id = Number(req.params.id);
+//             const user = users.find((user)=> user.id === id);
+//             return res.json(user);
+//         }
+//     )
+//    .patch((req,res)=>{
+//         //TODO: edit the user with id
+//         return res.json({ status: "pending" });
+//     })
+//     .delete((req,res)=>{
+//         //TODO: Delete the user with id
+//         return res.json({ status: "pending" });
+//     })
 
 app.listen(port,()=>console.log(`Server Started at port ${port}`));
