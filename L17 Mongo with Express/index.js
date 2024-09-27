ac20f7cf99417f07acfdcbaed73230b685f30910
codercastor <tejasshinde935@gmail.com>
2024-09-27 23:52:48 +0530

    commited

--- L17 Mongo with Express/index.js
+++ L17 Mongo with Express/index.js
@@ -122,6 +122,9 @@ app.post("/api/users", async (req, res) => {
   //   });
 });
 
+
+
+
 app.patch("/api/users/:id", async (req, res) => {
   //TODO: edit the user with id
   await User.findByIdAndUpdate(req.params.id, {
