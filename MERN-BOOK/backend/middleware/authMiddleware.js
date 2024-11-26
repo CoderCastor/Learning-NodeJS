9b8f770c43b74e2d5f8abaa98735aa5cf01ed0c6
codercastor <tejasshinde935@gmail.com>
2024-11-26 22:21:34 +0530

    saved

--- MERN-BOOK/backend/middleware/authMiddleware.js
+++ MERN-BOOK/backend/middleware/authMiddleware.js
@@ -10,8 +10,7 @@ const protect = (asyncHandler(async(req,res,next)=>{
             token = req.headers.authorization.split(' ')[1]
             const decoded = jwt.verify(token,process.env.JWT_SECRET)
             req.user = await User.findById(decoded.id).select('-password')
-            next();
-            
+            next(); 
         } catch (error) {
             console.log(error)
             res.status(401)
